23c4f851818be24702969471d51822bf65cbb127
chuntaichen <ct.chen@ischool.com.tw>
2020-06-08 17:05:19 +0800

    建立匯出匯入報表需要引用基本DLL與專案殼。

---
+++ MOD_siblings_ImportExport/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FISCA.Permission;
+using FISCA.Presentation.Controls;
+using K12.Data;
+using System.Windows.Forms;
+
+namespace MOD_siblings_ImportExport
+{
+    public class Program
+    {
+        [FISCA.MainMethod()]
+        public static void main()
+        {
+            //第一個 RibbonBar 功能
+            FISCA.Presentation.RibbonBarItem item = FISCA.Presentation.MotherForm.RibbonBarItems["學生", "康橋"];
+            item["第一個程式"].Enable = FISCA.Permission.UserAcl.Current["09613d9c-56b6-4511-b046-5b40689d5955"].Executable;
+            item["第一個程式"].Click += delegate
+            {
+                MessageBox.Show("Hello FISCA!");
+            };
+
+        }
+    }
+
+}
